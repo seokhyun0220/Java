75cff21e711ad47fd40f0277295441d867c2d6dd
seokhyun0220 <gyouj123@naver.com>
2021-05-17 11:36:09 +0900

    2021/05/17 Java Ch01 수업예제

---
+++ Ch01/src/sub1/HelloWorld.java
@@ -0,0 +1,14 @@
+package sub1;
+
+/*
+ * 날짜 : 2021/05/17
+ * 이름 : 강석현	
+ * 내용 : HelloWorld 출력하기
+ */
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World~");
+	}
+}
